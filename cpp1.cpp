b67f6647faa1e932508b46f6b30ceb52ab56dd28
Saurabh-del <gautamsaurabh199@gmail.com>
2021-10-03 11:03:22 +0530

    Optimed the code

--- cpp1.cpp
+++ cpp1.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    int n, i, m=0, flag=0;
+    int n,i, flag=0;
     cout << "Enter the Number to check Prime: ";
     cin >> n;
+
     if(n<2) {
         cout<<"Number is not Prime."<<endl;
     }
     else {
-        for(i = 2; i <= n/2; i++) {
+        for(i = 2; i*i <= n; i++) {
             if(n % i == 0) {
                 cout<<"Number is not Prime."<<endl;
                 flag=1;
